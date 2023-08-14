dbd5c677aea74480208f6e8a0d76aab4fea1cf57
Bogatyrev Roman <r.pittchai@yandex.ru>
2023-08-14 17:01:43 +0300

    1

--- app/unit1.pas
+++ app/unit1.pas
@@ -48,9 +48,9 @@ implementation
 
 procedure OutDNA(
     dna_nucl: dna_nucl_arr;
-    dna_result: String;
     Label5: TLabel;
-    StringGrid1: TStringGrid);
+    StringGrid1: TStringGrid;
+    length_res: integer);
 
 var
   gc_percent, factor: real;
@@ -58,62 +58,59 @@ var
 
 begin
   factor := Power(10.0, 2);
-  gc_percent := ((dna_nucl[2] + dna_nucl[3]) / length(dna_result)) * 100;
+  gc_percent := ((dna_nucl[2] + dna_nucl[3]) / length_res) * 100;
   gc_percent := Round(gc_percent * factor) / factor;
   Label5.Caption := (FloatToStr(gc_percent)) + '%';
   for i := 1 to StringGrid1.ColCount - 2 do
     for j := 1 to StringGrid1.RowCount -1 do
       begin
         StringGrid1.Cells[i, j] := FloatToStr(dna_nucl[j - 1]);
-        gc_percent := ((dna_nucl[j - 1]) / length(dna_result)) * 100;
+        gc_percent := ((dna_nucl[j - 1]) / length_res) * 100;
         gc_percent := Round(gc_percent * factor) / factor;
         StringGrid1.Cells[i + 1, j] := FloatToStr(gc_percent);
       end;
+  SetLength(dna_nucl, 0)
 end;
 
 procedure LenNucl(
-    dna_base, dna_result: String;
+    dna_result: String;
     Label5: TLabel; StringGrid1: TStringGrid);
 
 var
   dna_nucl: dna_nucl_arr;
-  i: integer;
+  i, length_res: integer;
 
 begin
   dna_nucl := [0.0, 0.0, 0.0, 0.0, 0.0];
-  for i := 1 to length(dna_result) do
+  length_res := length(dna_result);
+  for i := 1 to length_res do
     begin
-      begin
-        if dna_result[i] = dna_base[1] then
-          dna_nucl[0] := dna_nucl[0] + 1.0 // "A"
-        else if dna_result[i] = dna_base[2] then
-          dna_nucl[1] := dna_nucl[1] + 1.0 // "T"
-        else if dna_result[i] = dna_base[3] then
-          dna_nucl[2] := dna_nucl[2] + 1.0 // "G"
-        else if dna_result[i] = dna_base[4] then
-          dna_nucl[3] := dna_nucl[3] + 1.0 // "C"
-        else if Pos(dna_result[i], dna_base) = 0 then
-          dna_nucl[4] := dna_nucl[4] + 1.0 // "Other"
+      case dna_result[i] of
+        'A': dna_nucl[0] := dna_nucl[0] + 1.0;
+        'T': dna_nucl[1] := dna_nucl[1] + 1.0;
+        'G': dna_nucl[2] := dna_nucl[2] + 1.0;
+        'C': dna_nucl[3] := dna_nucl[3] + 1.0;
+        else
+          dna_nucl[4] := dna_nucl[4] + 1.0;
       end;
     end;
-  OutDNA(dna_nucl, dna_result, Label5, StringGrid1);
+  OutDNA(dna_nucl, Label5, StringGrid1, length_res);
+  SetLength(dna_result, 0)
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 
 var
   dna_result: String;
-  dna_base: String;
 
 begin
-  dna_base := 'ATGC';
   dna_result := StringReplace(Memo1.Lines.Text, sLineBreak, '', [rfReplaceAll]);
   if length(dna_result) <= 0 then
     Edit1.Text := 'Empty'
   else
     begin
       Edit1.Text := IntToStr(length(dna_result));
-      LenNucl(dna_base, dna_result, Label5, StringGrid1);
+      LenNucl(dna_result, Label5, StringGrid1);
     end;
 end;
 
@@ -127,7 +124,7 @@ begin
   Label5.Caption := '0.0 %';
   Memo1.Clear;
   for i := 1 to StringGrid1.ColCount - 1 do
-    for j := 1 to StringGrid1.RowCount -1 do
+    for j := 1 to StringGrid1.RowCount - 1 do
       begin
         StringGrid1.Cells[i, j] := '';
       end;
