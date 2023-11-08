84b6115ecd353625916bd776d19bd86a5f45180d
Bogatyrev Roman <r.pittchai@gmail.com>
2023-11-08 21:39:35 +0300

    bio

--- app/GC Content Calculator/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    // Here will be code
-    return 0;
-}
---
+++ app/GC Content Calculator/main.f90
@@ -0,0 +1,8 @@
+
+program hello
+    implicit none
+
+    print *, "Here will be code"
+
+end program
+
