31f9c082c2891a5d8aa63e2b5c84743be06ef96d
r.pittchai <r.pittchai@yandex.ru>
2023-11-23 13:22:23 +0300

    int

--- apps/GC content calculator/app/main.c
+++ apps/GC content calculator/app/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "nucleotide_functions.h"
 
 #define BUFFER_SIZE 8192 // Buffer size
 #define NUMBER_OF_NUCLEOTIDES 5 // Number of nucleotides
@@ -21,7 +22,7 @@ int main() {
     float array_percent_of_nucleotides[NUMBER_OF_NUCLEOTIDES] = {0.0, 0.0, 0.0, 0.0, 0.0};
     FILE *f;
 
-    f = fopen("TEST_DNA", "r");
+    f = fopen("/home/pittchai/DEV/Bio/apps/GC content calculator/Test files/TEST_DNA", "r");
     if (f == NULL) {
         printf("Error opening file\n");
         return 1;
@@ -68,26 +69,4 @@ int main() {
     printf("Number of GC content in percent - %0.2f%\n", gc_content_percent);
 
     return 0;
-}
-
-
-void gc_percent(long int *array_number_of_nucleotides, \
-                float *gc_content_percent, long int *length_dna, \
-                long int *gc_content)
-{   // Search GC percent
-    *gc_content = ((float)array_number_of_nucleotides[2] + (float)array_number_of_nucleotides[3]);
-    *gc_content_percent = (((float)array_number_of_nucleotides[2]
-                     + (float)array_number_of_nucleotides[3])
-                     / (float)(*length_dna)) * 100;
-}
-
-
-void percent_nucl(float *array_percent_of_nucleotides, \
-                  long int *array_number_of_nucleotides, \
-                  long int *length_dna)
-{   // Search percent of nucleotides
-    for (int i = 0; i < NUMBER_OF_NUCLEOTIDES; i++) {
-        array_percent_of_nucleotides[i] = ((float)array_number_of_nucleotides[i]
-                                            / (float)(*length_dna)) * 100;
-    }
 }
---
+++ apps/GC content calculator/app/nucleotide_functions.h
@@ -0,0 +1,7 @@
+#ifndef NUCLEOTIDE_FUNCTIONS_H_INCLUDED
+#define NUCLEOTIDE_FUNCTIONS_H_INCLUDED
+
+void gc_percent(long int *array_number_of_nucleotides, float *gc_content_percent, long int *length_dna, long int *gc_content);
+void percent_nucl(float *array_percent_of_nucleotides, long int *array_number_of_nucleotides, long int *length_dna);
+
+#endif // NUCLEOTIDE_FUNCTIONS_H_INCLUDED
